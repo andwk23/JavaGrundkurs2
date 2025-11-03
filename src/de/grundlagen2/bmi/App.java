606258485f31aa91cd3cdf6e5554488304f7de1e
andwk23 <fuerschrott@online.de>
2025-11-03 18:31:46 +0100

    EVA-Prinzip

--- src/de/grundlagen2/bmi/App.java
+++ src/de/grundlagen2/bmi/App.java
@@ -5,17 +5,38 @@ import de.grundlagen2.bmi.model.Katze;
 import de.grundlagen2.bmi.model.Lebewesen;
 
 public class App {
+	
+	private Lebewesen lebewesen;
+	private String ergebnis;
 
 	public static void main(String[] args) {
-		
-		Lebewesen lebewesen = new Katze();
+		new App();
+	}
+	
+	// Konstruktor
+	private App() {
+		System.out.println("BMI [Programm ist gestartet]");
+		gibEin();
+		verarbeite();
+		gibAus();
+		System.out.println("BMI [Programm ist beendet]");
+	}
+	
+	// Umbau nach dem EVA-Prinzip (Eingabe, Verarbeite, Ausgabe)
+
+	private void gibEin() {			// E
+		lebewesen = new Katze();
 		lebewesen.setName("Pinki");
 		lebewesen.setGroesse(0.51);
 		lebewesen.setGewicht(6.1);
-		
+	}
+
+	private void verarbeite() {		// V
 		BMIRechner rechner = new BMIRechner();
-		String ergebnis = rechner.pruefe(lebewesen);
-		
+		ergebnis = rechner.pruefe(lebewesen);
+	}
+	
+	private void gibAus() {			// A
 		System.out.println("BMI [ergebnis = " + ergebnis + "]");
 	}
 
