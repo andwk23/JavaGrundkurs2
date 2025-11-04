f68577ffd593b181be76b52aa6bf798ae295a588
andwk23 <fuerschrott@online.de>
2025-11-04 18:07:34 +0100

    Menue, instanceof

--- src/de/grundlagen2/bmi/App.java
+++ src/de/grundlagen2/bmi/App.java
@@ -3,6 +3,7 @@ package de.grundlagen2.bmi;
 import java.util.Scanner;
 
 import de.grundlagen2.bmi.controller.BMIRechner;
+import de.grundlagen2.bmi.model.Fisch;
 import de.grundlagen2.bmi.model.Katze;
 import de.grundlagen2.bmi.model.Lebewesen;
 
@@ -19,9 +20,29 @@ public class App {
 	// Konstruktor
 	private App() {
 		System.out.println("BMI [Programm ist gestartet]");
-		gibEin();
-		verarbeite();
-		gibAus();
+		
+		while (true) {
+			System.out.print("BMI [1=Eingabe 2=Verarbeiten 3=Ausgabe 4=Beenden] > ");
+			int auswahl = scanner.nextInt();
+			
+			if(auswahl == 1) {
+				gibEin();
+			}
+			else if(auswahl == 2) {
+				verarbeite();
+			}
+			else if(auswahl == 3) {
+				gibAus();
+			}
+			else if(auswahl == 4) {
+				break;
+			}
+
+			else {
+				System.out.println("BMI [Falsche Eingabe!]");
+			}
+		}
+		
 		scanner.close();
 		System.out.println("BMI [Programm ist beendet]");
 	}
@@ -30,7 +51,31 @@ public class App {
 
 	private void gibEin() {			// E
 		
-		lebewesen = new Katze();
+		
+		while(true) {
+			System.out.print("BMI [1=Katze 2=Fisch] > ");			
+			int lebewesenTyp = scanner.nextInt();
+			
+			if(lebewesenTyp == 1) {
+				lebewesen = new Katze();
+				break;
+			}
+			else if(lebewesenTyp == 2) {
+				lebewesen = new Fisch();
+				break;
+			}
+
+			else {
+				System.out.println("BMI [Falsche Eingabe!]");
+			}
+		}
+		
+		if(lebewesen instanceof Katze) {
+			System.out.println("BMI [Neue Katze]");
+		}
+		else if(lebewesen instanceof Fisch) {
+			System.out.println("BMI [Neuer Fisch]");
+		}
 		
 		System.out.print("BMI [Geben Sie den Namen ein] > ");
 		String name = scanner.next();
